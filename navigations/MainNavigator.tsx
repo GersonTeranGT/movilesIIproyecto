6b4767bc57f164083e537cf1aa7252ccaebfad5e
EduTama123 <edtamayo@itsqmet.edu.ec>
2026-01-15 01:23:27 -0500

    implementacion de ventanas menu, videojuego y puntuaciones

--- navigations/MainNavigator.tsx
+++ navigations/MainNavigator.tsx
@@ -4,42 +4,55 @@ import FormScreen from "../screens/FormScreen";
 import LoginScreen from "../screens/LoginScreen";
 import JuegoScreen from "../screens/JuegoScreen";
 import { NavigationContainer } from "@react-navigation/native";
+import GameScreen from "../screens/GamesScreen";
+import PuntuacionesScreen from "../screens/PuntuacionesScreen";
+
+//icons
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-function MyStack(){
-    return(
+function MyStack() {
+    return (
         <Stack.Navigator
-        screenOptions={{
-            headerStyle:{
-                backgroundColor: "#233D4D",
-            },
-            headerTintColor: '#FFFFFF',
-            headerTitleStyle: {
-                fontWeight: 'bold',
-            },
-        }}
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: "#233D4D",
+                },
+                headerTintColor: '#FFFFFF',
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                },
+            }}
         >
-            <Stack.Screen name="Formulario" component={FormScreen}/>
-            <Stack.Screen name="Login" component={LoginScreen}/>
-            <Stack.Screen name="Tab" component={MyTab}/>
+            <Stack.Screen name="Formulario" component={FormScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Tab" component={MyTab} />
         </Stack.Navigator>
     )
 }
 
-function MyTab(){
-    return(
+function MyTab() {
+    return (
         <Tab.Navigator>
-            <Tab.Screen name="Juego" component={JuegoScreen}/>
+            <Tab.Screen name="Menú" component={JuegoScreen} 
+             options={{tabBarIcon: ()=> <MaterialIcons name="menu" size={24} color="#0059ff" /> }}
+            />
+            <Tab.Screen name="Juego" component={GameScreen} 
+             options={{tabBarIcon: ()=> <MaterialIcons name="sports-esports" size={24} color="#ff0000" /> }}
+            />
+            <Tab.Screen name="Puntuación" component={PuntuacionesScreen} 
+             options={{tabBarIcon: ()=> <MaterialIcons name="leaderboard" size={24} color="#44ff00" /> }}
+            />
         </Tab.Navigator>
     )
 }
 
-export default function MainNav(){
+export default function MainNav() {
     return (
         <NavigationContainer>
-            <MyStack/>
+            <MyStack />
         </NavigationContainer>
     )
 }
